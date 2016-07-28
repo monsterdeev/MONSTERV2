124857c13df34bebd82b13060780c1027db806a8
hasan <hasan.muhafada@yahoo.com>
2016-07-28 15:33:29 +0300

    Update lock_fwd.lua

--- plugins/lock_fwd.lua
+++ plugins/lock_fwd.lua
@@ -1,24 +1,11 @@
---[[
-#
-#ـــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ
-#:((
-# For More Information ....! 
-# Developer : Aziz < @TH3_GHOST > 
-# our channel: @DevPointTeam
-# Version: 1.1
-#:))
-#ــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ
-#
-]]
 do
 
 local function pre_process(msg)
     
     --Checking mute
-    local hash = 'mate:'..msg.to.id
-    if redis:get(hash) and msg.fwd_from and not is_sudo(msg) and not is_owner(msg) and not is_momod(msg) and not is_admin1(msg)  then
+    local hashg = 'mateu:'..msg.to.id
+    if redis:get(hashg) and msg.fwd_from and not is_sudo(msg) and not is_owner(msg) and not is_momod(msg) and not is_admin1(msg)  then
             delete_msg(msg.id, ok_cb, true)
-            send_large_msg(get_receiver(msg), 'عزيزي '..msg.from.first_name..'\nممنوع عمل اعادة توجيه من القنوات هنا التزم بقوانين المجموعة 👋👮\n#username @'..msg.from.username)
             return "done"
         end
     
@@ -28,18 +15,18 @@ local function pre_process(msg)
   
 
 
-local function DevPoint(msg, matches)
+local function run(msg, matches)
     chat_id =  msg.to.id
     
     if is_momod(msg) and matches[1] == 'lock' then
       
             
-                    local hash = 'mate:'..msg.to.id
-                    redis:set(hash, true)
+                    local hashg = 'mateu:'..msg.to.id
+                    redis:set(hashg, true)
                     return ""
   elseif is_momod(msg) and matches[1] == 'unlock' then
-                    local hash = 'mate:'..msg.to.id
-                    redis:del(hash)
+                    local hashg = 'mateu:'..msg.to.id
+                    redis:del(hashg)
                     return ""
 end
 
@@ -50,7 +37,7 @@ return {
         '^[/!#](lock) fwd$',
         '^[/!#](unlock) fwd$'
     },
-    run = DevPoint,
+    run = run,
     pre_process = pre_process
 }
 end
